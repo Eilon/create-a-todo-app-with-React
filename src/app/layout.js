0e254b21ddb74133c317de69fa7e20a021f102a7
Eilon Lipton <Eilon@users.noreply.github.com>
2024-11-11 14:10:26 -0800

    Set viewport for better mobile device layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,7 +13,8 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <script src="HybridWebView.js"></script>
-      </head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover" />
+        </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
